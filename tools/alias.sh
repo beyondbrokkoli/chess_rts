4f9f34ba5f0cc68665c85c0b27c588198557a316
beyondbrokkoli <halim.zag@gmx.de>
2026-08-07 23:28:01 +0200

    m

--- tools/alias.sh
+++
@@ -1,1 +0,0 @@
-alias zramstat='echo -e "Orig_Data(B) Compr_Data(B) Mem_Total(B) Mem_Limit(B) Max_Used(B) Same_Pages Pages_Compacted Huge_Pages Huge_Pages_Since\n$(cat /sys/block/zram0/mm_stat)" | column -t'
